4260605d72d2fcff32e4aefce2f28e43415177a8
Kenjicci <Cincokencstd1@gmail.com>
2024-09-18 19:09:38 +0800

    added basic html form structure

---
+++ PHP PART 1/PS 01.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Area of a Triangle</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Area of a Triangle</h1>
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="sidea">Side a: </label>
+                <input type="number" id="sidea" name="sidea" step="0.01" required>
+            </div>
+            <div class="form-group">
+                <label for="sideb">Side b: </label>
+                <input type="number" id="sideb" name="sideb" step="0.01" required>
+            </div>
+            <div class="form-group">
+                <label for="sidec">Side c: </label>
+                <input type="number" id="sidec" name="sidec" step="0.01" required>
+            </div>
+            <button type="submit">Calculate</button>
+        </form>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PHP-APPDEV
+Contains Week 6-7 set of activities in Application Development and Emerging Technologies  
+
+Authors:
+Auth1: Ken C. Cinco  
+Username: Kenjicci  
+
+
+Auth2: Frances Bea V. Magdayao  
+Username: ampalayaa  
